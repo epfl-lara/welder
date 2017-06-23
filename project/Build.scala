198777d1bdf800a0d4d8c086760840189bc40a1c
Romain Edelmann <romain.edelmann@gmail.com>
2017-06-23 11:46:14 +0200

    Update the version of Inox used.

--- project/Build.scala
+++ project/Build.scala
@@ -4,6 +4,6 @@ import sbt._
 object WelderBuild extends Build {
 
   lazy val root = Project("root", file(".")) dependsOn(inoxProject)
-  lazy val inoxProject = RootProject(uri("git://github.com/epfl-lara/inox.git#78ab430ccc9cc1fe45bcaae864fb129f7fa542fe"))
+  lazy val inoxProject = RootProject(uri("git://github.com/epfl-lara/inox.git#fc2ec280736a8cdd1322631c3bbeacd28411f6c3"))
 
 }
