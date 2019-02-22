4816525c10790d8b4ce5a9c395573c372d6cbe6f
Romain Edelmann <romain.edelmann@gmail.com>
2019-02-22 14:16:27 +0100

    Start working on new "solve" internals

--- src/main/scala/welder/Solvers.scala
+++ src/main/scala/welder/Solvers.scala
@@ -4,7 +4,6 @@ package welder
 
 import inox.solvers
 import inox.solvers._
-import inox.transformers._
 
 /** Contains methods related to SMT solvers. */
 trait Solvers { self: Theory =>
@@ -16,48 +15,53 @@ trait Solvers { self: Theory =>
   // Factory of solvers.
   private lazy val factory = solvers.SolverFactory(program, ctx)
 
-  object assumptionsCollector {
-    def collect(expr: Expr): Seq[() => Boolean] = program.symbols.collectWithPC(expr)  {
-      case (Assume(cond, _), path) => () => checkNegationUnsat(cond, path.conditions)
-      case (cons@ADT(id, tps, exprs), path) if cons.getConstructor.sort.hasInvariant => {
-        val sort = cons.getConstructor.sort
-        val invariantApply = FunctionInvocation(sort.invariant.get.id, sort.tps, Seq(cons))
-        () => checkNegationUnsat(invariantApply, path.conditions)
-      }
-      case (Choose(vd, pred), path) => {
-        val vdFresh = vd.freshen
-        val conditions = path.conditions.map(exprOps.replaceFromSymbols(Map(vd -> vdFresh.toVariable), _))
-        val freeVars = (pred +: conditions).map(exprOps.variablesOf(_)).reduce(_ ++ _).map(_.toVal) - vd
-        // Not supported by Inox:
-        // val forall = Forall(freeVars.toSeq, implies(and(conditions : _*), not(Forall(Seq(vd), not(pred)))))
-        // () => checkNegationUnsat(forall, Seq())
-        if (freeVars.isEmpty) {
-          println(implies(and(conditions : _*), pred))
-          () => checkSat(implies(and(conditions : _*), pred))
-        }
-        else {
-          // TODO: Can we do better here ?
-          () => false
-        }
-      }
-      //    case (AsInstanceOf(expr, tpe), path) => {
-      //      () => checkNegationUnsat(IsInstanceOf(expr, tpe), path.conditions)
-      //    }
-      case (Division(lhs, rhs), path) => {
-        () => checkNegationUnsat(Not(Equals(rhs, zero(rhs.getType))), path.conditions)
-      }
-      case (Remainder(lhs, rhs), path) => {
-        () => checkNegationUnsat(Not(Equals(rhs, zero(rhs.getType))), path.conditions)
-      }
-      case (Modulo(lhs, rhs), path) => {
-        () => checkNegationUnsat(Not(Equals(rhs, zero(rhs.getType))), path.conditions)
-      }
-      case (FractionLiteral(_, d), _) => {
-        () => d != 0
-      }
-      // TODO: Function calls.
-    }
+  private def collectConditions(expr: Expr): Seq[(Expr, Seq[Expr])] = program.symbols.collectWithPC(expr, true) {
+    case (Division(lhs, rhs), path) => (not(equality(rhs, zero(rhs.getType))), path.conditions)
+    case (Remainder(lhs, rhs), path) => (not(equality(rhs, zero(rhs.getType))), path.conditions)
+    case (Modulo(lhs, rhs), path) => (not(equality(rhs, zero(rhs.getType))), path.conditions)
+    case (FractionLiteral(_, d), path) if d == 0 => (BooleanLiteral(false), path.conditions)
   }
+
+//  def collect(expr: Expr, assumptions: Seq[Theorem]): Seq[() => Boolean] = program.symbols.collectWithPC(expr)  {
+//    case (Assume(cond, _), path) => () => checkNegationUnsat(cond, path.conditions ++ assumptions.map(_.expression))
+//    case (cons@ADT(id, tps, exprs), path) if cons.getConstructor.sort.hasInvariant => {
+//      val sort = cons.getConstructor.sort
+//      val invariantApply = FunctionInvocation(sort.invariant.get.id, sort.tps, Seq(cons))
+//      () => checkNegationUnsat(invariantApply, path.conditions ++ assumptions.map(_.expression))
+//    }
+//    case (Choose(vd, pred), path) => {
+//      val vdFresh = vd.freshen
+//      val conditions = path.conditions.map(exprOps.replaceFromSymbols(Map(vd -> vdFresh.toVariable), _))
+//      val freeVars = (pred +: conditions).map(exprOps.variablesOf(_)).reduce(_ ++ _).map(_.toVal) - vd
+//      // Not supported by Inox:
+//      // val forall = Forall(freeVars.toSeq, implies(and(conditions : _*), not(Forall(Seq(vd), not(pred)))))
+//      // () => checkNegationUnsat(forall, Seq())
+//      if (freeVars.isEmpty) {
+//        println(implies(and(conditions : _*), pred))
+//        () => checkSat(implies(and(conditions : _*), pred))
+//      }
+//      else {
+//        // TODO: Can we do better here ?
+//        () => false
+//      }
+//    }
+//    //    case (AsInstanceOf(expr, tpe), path) => {
+//    //      () => checkNegationUnsat(IsInstanceOf(expr, tpe), path.conditions)
+//    //    }
+//    case (Division(lhs, rhs), path) => {
+//      () => checkNegationUnsat(Not(Equals(rhs, zero(rhs.getType))), path.conditions ++ assumptions.map(_.expression))
+//    }
+//    case (Remainder(lhs, rhs), path) => {
+//      () => checkNegationUnsat(Not(Equals(rhs, zero(rhs.getType))), path.conditions ++ assumptions.map(_.expression))
+//    }
+//    case (Modulo(lhs, rhs), path) => {
+//      () => checkNegationUnsat(Not(Equals(rhs, zero(rhs.getType))), path.conditions ++ assumptions.map(_.expression))
+//    }
+//    case (FractionLiteral(_, d), _) => {
+//      () => d != 0
+//    }
+//    // TODO: Function calls.
+//  }
 
   private def zero(tpe: Type): Expr = tpe match {
     case IntegerType() => IntegerLiteral(0)
@@ -90,6 +94,7 @@ trait Solvers { self: Theory =>
    * @return The expression `expr` as a `Theorem`.
    */ 
   def prove(expr: Expr, assumptions: Seq[Theorem]): Attempt[Theorem] = {
+
     if (expr.getType != BooleanType()) {
       Attempt.typeError("prove", expr.getType)
     }
@@ -97,13 +102,18 @@ trait Solvers { self: Theory =>
       val hypotheses = assumptions.map(_.expression)
 
       if (checkNegationUnsat(expr, hypotheses)) {
-        for (check <- assumptionsCollector.collect(expr)) {
-          if (!check()) {
-            return Attempt.fail("SMT solver could not prove an assumption made by the expression " + expr + ".")
-          }
+        val conditions = collectConditions(expr)
+        val attempts = conditions.collect {
+          case (condition, pathConditions) if !checkNegationUnsat(condition, pathConditions ++ hypotheses) =>
+            val rest = if (pathConditions.isEmpty && hypotheses.isEmpty) "." else
+              " using the following hypotheses: " + (pathConditions ++ hypotheses).mkString(", ") + "."
+            Attempt.fail("SMT solver could not prove assumption " + condition + " made by expression " + expr + rest)
         }
 
-        Attempt.success(new Theorem(expr).from(assumptions))
+        for {
+          _ <- Attempt.all(attempts)
+          t <- Attempt.success(new Theorem(expr).from(assumptions))
+        } yield t
       }
       else {
         Attempt.fail("SMT solver could not prove the property: " + expr +
